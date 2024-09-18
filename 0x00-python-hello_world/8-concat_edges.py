c5b0f71a47f80da6543085ead0918ee214436009
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-09-18 06:43:54 +0100

    NEW concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-concat = str[39:66] + str[106:112] + str[:7]
+concat = str[39:66] + str[106:112] + str[:6]
 print(concat)
