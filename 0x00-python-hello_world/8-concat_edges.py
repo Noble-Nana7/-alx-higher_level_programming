64f8f9fe3e62f44f0828f72ef3c19a7147919d6d
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-09-18 06:34:54 +0100

    edge concat

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
+print(str[39:65] + str[105:112] + str[:7])
