3e9341b19cbee102ee61e7935bf1b2951f1ca76a
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-09-27 00:00:16 +0100

    To uppercase, yay

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    result = ""
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
-        print(f"{i}", end="")
+        print("{:s}".format(i), end="")
     print()
-
-str = "what is you name"
-uppercase(str)
