8c8878ade2b8db9f8c454f696617e2df68beee08
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-16 17:15:30 +0100

    Everything can be imported!

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    names.sort()
+
+    for name in names:
+        if name == *__:
+            continue:
+                else:
+                    print(name)
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "_main__":
+
+    import a from variable_load_5
+
+    print(a)
