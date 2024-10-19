b58c6fbfb9285102e2f6e9a465b6c25043242bef
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-19 03:45:55 +0100

    I can C you!

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    if string:
+        new_string = ""
+        for i in string:
+            if i != 'c' and i != 'C':
+                new_string += i
+            return new_string
+
