3fb8d3c76fa2c1501ca088760bf1c2c5fca406a5
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-19 13:41:18 +0100

    if c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,9 @@
 def no_c(my_string):
     if my_string == "":
         return ""
-
+    else:
         new_string = ""
-        for i in my_string:
-            if i != 'c' and i != 'C':
+        for i in string:
+            if i != 'C' and i != 'c':
                 new_string += i
         return new_string
