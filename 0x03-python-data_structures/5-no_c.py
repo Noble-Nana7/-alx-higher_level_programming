e86a3550d4e2766f656ec994482cb379606b31a6
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-19 13:42:22 +0100

    i can c you!

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,7 @@ def no_c(my_string):
         return ""
     else:
         new_string = ""
-        for i in string:
+        for i in my_string:
             if i != 'C' and i != 'c':
                 new_string += i
         return new_string
