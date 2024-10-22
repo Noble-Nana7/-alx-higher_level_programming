ff278e586a3704517edc4d8990886ddd16b44c17
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-22 04:22:26 +0100

    Again!

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Use 0 if the tuple has fewer than 2 elements, otherwise use its actual values
+    # Use 0 if the tuple has fewer than 2 elements,
+    # otherwise use its actual values
     a1 = tuple_a[0] if len(tuple_a) > 0 else 0
     a2 = tuple_a[1] if len(tuple_a) > 1 else 0
     b1 = tuple_b[0] if len(tuple_b) > 0 else 0
     b2 = tuple_b[1] if len(tuple_b) > 1 else 0
-    
     # Add the corresponding elements from both tuples and return a new tuple
     return (a1 + b1, a2 + b2)
-
