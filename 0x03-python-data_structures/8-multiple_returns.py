906c0201380993d5e6dddc33f4c83b30b3aa28ef
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-22 04:55:15 +0100

    More Returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,5 @@
 
 def multiple_returns(sentence):
     if sentence:
-        print(sentence[0])
-        return len(sentence)
-    return None
+        return len(sentence), sentence[0]
+    return 0, None
