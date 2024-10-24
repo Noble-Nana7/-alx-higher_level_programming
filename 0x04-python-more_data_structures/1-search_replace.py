3819f47ac39a802d0d5ed5b0868ded13d0948270
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-24 03:13:11 +0100

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
+new_list = []
+
 def search_replace(my_list, search, replace):
     for idx in range(len(my_list)):
-        if my_list[idx] == search:
-            my_list[idx] = replace
-    return my_list
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+
+    return new_list
+        
