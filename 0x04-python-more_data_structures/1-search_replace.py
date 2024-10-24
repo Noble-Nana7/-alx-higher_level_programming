76390b58df6ea5dc2521cb8e2be0204d4eec2330
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-24 03:14:13 +0100

    Again!

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,10 +4,10 @@ new_list = []
 
 def search_replace(my_list, search, replace):
     for idx in range(len(my_list)):
-        if my_list[i] == search:
+        if my_list[idx] == search:
             new_list.append(replace)
         else:
-            new_list.append(my_list[i])
+            new_list.append(my_list[idx])
 
     return new_list
         
