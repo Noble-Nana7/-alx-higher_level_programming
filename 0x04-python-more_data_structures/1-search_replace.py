133882723a467fe0113f20c8b7b8ee359acc830c
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-24 03:15:29 +0100

    Hello, Searched and replaced!

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 new_list = []
-
 def search_replace(my_list, search, replace):
     for idx in range(len(my_list)):
         if my_list[idx] == search:
             new_list.append(replace)
         else:
             new_list.append(my_list[idx])
-
     return new_list
-        
