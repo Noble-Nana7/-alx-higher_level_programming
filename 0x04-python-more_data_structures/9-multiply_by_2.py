bf21b7b56515af318492582ab27ab5cda02182e9
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-26 02:10:34 +0100

    a bug

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@ def multiply_by_2(a_dictionary):
 
     new_dict = {}
 
-    for key, value in a_dictionary:
+    for key, value in a_dictionary.items():
         new_dict[key] = value * 2
 
     return new_dict
