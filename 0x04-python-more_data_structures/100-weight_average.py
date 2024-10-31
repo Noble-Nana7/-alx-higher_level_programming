76b8a08047bf9d0afe8aeef2f1e5ac82f32a341e
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-31 01:03:51 +0100

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    weight_sum = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        weight_sum += score * weight
+        total_weight += weight
+
+    return weight_sum  / total_weight if total_weight != 0 else 0
