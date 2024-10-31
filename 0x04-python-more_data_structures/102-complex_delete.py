f0a5e0bc93d59db798f389d6126025797e0345d5
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-10-31 02:09:26 +0100

    Hello

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+    if a_dictionary is None or not isinstance(a_dictionary, dict):
+    return  # or handle the error accordingly
+
+
     keys = [key for key, val in a_dictionary.items() if val == value]
     for key in keys:
         del a_dictionary[key]
