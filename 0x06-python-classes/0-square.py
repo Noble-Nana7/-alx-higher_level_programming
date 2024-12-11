59ab90eb9a95fd813ad695ca0071131430db1791
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-12-11 03:59:36 +0100

    Okay!

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square that represents a square.
+The class currently doesn't have any attributes or methods but serves
+as a basic template for defining a square.
+"""
 
 class Square:
     """
-    A class to define a square
+    A class to define a square.
     """
-    pass`
+    pass
+
+if __name__ == "__main__":
+    """Main entry point when this module is run as a script."""
+    square = Square()
