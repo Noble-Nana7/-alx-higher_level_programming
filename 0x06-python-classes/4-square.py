67db2a20e0b3cd0fa62a46368fd5a974f5ed96fb
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-12-17 03:44:20 +0100

    Fixed a Bug

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -51,7 +51,7 @@ class Sqaure:
             """
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-            if size < 0:
+            if value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
